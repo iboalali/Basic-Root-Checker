632c5964196917715b4b180017de13dcfd6bdc8c
Ibrahim Al-Alali <iboalali@gmail.com>
2017-11-17 23:33:43 +0100

    Billing Manager

---
+++ app/src/main/java/com/iboalali/basicrootchecker/BillingConstants.java
@@ -0,0 +1,19 @@
+package com.iboalali.basicrootchecker;
+
+/**
+ * Created by iboalali on 17-Nov-17.
+ */
+
+public final class BillingConstants {
+    /**
+     * Static fields and methods useful for billing
+     */
+
+    // this SKU is consumable
+    public static final String SKU_DONATION = "small_donation";
+
+    // list of all SKUs in this app
+    private static final String[] IN_APP_SKU = {SKU_DONATION};
+
+    private BillingConstants(){}
+}
