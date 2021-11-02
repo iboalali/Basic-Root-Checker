a7cfdcc7411d874af9a76b6e92d6f2b52802b708
Ibrahim Al-Alali <alali@live.de>
2021-11-02 22:54:21 +0100

    Fixed animated icon not animating to the end.
    Fixed theme color for night mode

--- app/src/main/java/com/iboalali/basicrootchecker/MainActivity.java
+++ app/src/main/java/com/iboalali/basicrootchecker/MainActivity.java
@@ -1,8 +1,13 @@
 package com.iboalali.basicrootchecker;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,6 +15,7 @@ import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import androidx.databinding.DataBindingUtil;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -17,6 +23,8 @@ import com.iboalali.basicrootchecker.components.RootChecker;
 import com.iboalali.basicrootchecker.components.RootCheckerContract;
 import com.iboalali.basicrootchecker.databinding.ActivityMainBinding;
 import com.jaredrummler.android.device.DeviceName;
+
+import java.lang.reflect.Field;
 
 public class MainActivity extends AppCompatActivity implements RootCheckerContract {
 
@@ -26,13 +34,86 @@ public class MainActivity extends AppCompatActivity implements RootCheckerContra
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        SplashScreen.installSplashScreen(this);
+        SplashScreen.installSplashScreen(this)
+                .setOnExitAnimationListener(splashScreenProvider -> {
+                    Log.d("SplashScreen", String.format("currentTime is %s ", System.currentTimeMillis()));
+
+                    long startMillis = splashScreenProvider.getIconAnimationStartMillis();
+                    Log.d("SplashScreen", String.format("startMillis is %s ", startMillis));
+
+                    if (startMillis == 0) {
+                        splashScreenProvider.remove();
+                        return;
+                    }
+
+                    long timeDiff = startMillis - System.currentTimeMillis();
+                    Log.d("SplashScreen", String.format("timeDiff is %s ", timeDiff));
+                    Log.d("SplashScreen", String.format("animation duration is %s ", splashScreenProvider.getIconAnimationDurationMillis()));
+
+                    long exitTimeDelay;
+                    if (timeDiff <= 0) {
+                        exitTimeDelay = splashScreenProvider.getIconAnimationDurationMillis() + timeDiff;
+                    } else {
+                        exitTimeDelay = splashScreenProvider.getIconAnimationDurationMillis();
+                    }
+
+                    Log.d("SplashScreen", String.format("exitTimeDelay is %s ", exitTimeDelay));
+                    AlphaAnimation alphaAnimation = new AlphaAnimation(1f, 0f);
+                    alphaAnimation.setDuration(200);
+                    alphaAnimation.setStartOffset(exitTimeDelay);
+                    alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            Log.d("SplashScreen", "animation ended");
+                            splashScreenProvider.remove();
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+
+                    Log.d("SplashScreen", "start hiding splash screen animation");
+                    splashScreenProvider.getView().startAnimation(alphaAnimation);
+                });
+
+        try {
+            // for debugging purposes
+            //
+            //noinspection ConstantConditions
+            Field themeIdField = getClass().getSuperclass().getSuperclass().getSuperclass().getSuperclass().getSuperclass().getSuperclass().getDeclaredField("mThemeResource");
+            themeIdField.setAccessible(true);
+            //noinspection ConstantConditions
+            int themeId = (int) themeIdField.get(this);
+            Log.d("SplashScreen", "onCreate: theme id = " + themeId);
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                boolean lightStatusBar = (this.getWindow().getDecorView().getSystemUiVisibility() & View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR) == View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+                Log.d("SplashScreen", "onCreate: lightStatusBar = " + lightStatusBar);
+            }
+        } catch (Exception e) {
+            Log.d("SplashScreen", "couldn't read the theme id");
+            e.printStackTrace();
+        }
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         initInstances();
     }
 
     private void initInstances() {
+        // Start of workaround:
+        // this is here because when the splash screen theme is being used, the status bar is not in
+        // light mode, event though reading it's value does indicate that it is set to light mode
+        boolean isNight = (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_YES) == Configuration.UI_MODE_NIGHT_YES;
+        new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView()).setAppearanceLightStatusBars(!isNight);
+        // End of workaround
+
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         binding.appToolbar.setOnMenuItemClickListener(item -> {
             int id = item.getItemId();
