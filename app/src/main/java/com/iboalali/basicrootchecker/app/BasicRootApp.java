dedb61a45c292dd9ae16780759a631f3dbf21218
al-alali <iboalali@gmail.com>
2022-10-10 21:31:49 +0200

    Added dynamics colors to all activities
    except the main activity, because of the splash screen

---
+++ app/src/main/java/com/iboalali/basicrootchecker/app/BasicRootApp.java
@@ -0,0 +1,15 @@
+package com.iboalali.basicrootchecker.app;
+
+import android.app.Application;
+
+import com.google.android.material.color.DynamicColors;
+
+public class BasicRootApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
