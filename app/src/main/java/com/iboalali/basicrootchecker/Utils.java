61b7cdf95efe1be383e80656d54e3fc99faabcfa
Ibrahim Al-Alali <iboalali@gmail.com>
2025-10-02 11:36:19 +0200

    refactor: remove unused utility methods
    The following unused methods have been removed from the `Utils` class:
    *   `equals(Object a, Object b)`
    *   `setSystemWindowLightMode(Activity activity, boolean on)`
    
    Additionally, error logging in `getAppVersionNumber` has been changed from `e.printStackTrace()` to `Log.e()`.

--- app/src/main/java/com/iboalali/basicrootchecker/Utils.java
+++ app/src/main/java/com/iboalali/basicrootchecker/Utils.java
@@ -1,15 +1,13 @@
 package com.iboalali.basicrootchecker;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.os.Build;
-import android.view.View;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 /**
  * Created by Ibrahim on 17-Jul-15.
@@ -22,12 +20,12 @@ final class Utils {
             PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
             return info.versionName;
         } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
+            Log.e("Units", "getAppVersionNumber: ", e);
         }
         return "";
     }
 
-    static String getAndroidName(Resources resources) {
+    static String getAndroidName(@NonNull Resources resources) {
         String[] versionNames = resources.getStringArray(R.array.VersionNames);
 
         if (Build.VERSION.SDK_INT > versionNames.length) {
@@ -35,26 +33,5 @@ final class Utils {
         }
 
         return versionNames[Build.VERSION.SDK_INT - 1];
-    }
-
-    @SuppressWarnings("EqualsReplaceableByObjectsCall")
-    static boolean equals(@Nullable Object a, @Nullable Object b) {
-        return (a == b) || (a != null && a.equals(b));
-    }
-
-    static void setSystemWindowLightMode(@NonNull Activity activity, boolean on) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-
-            View view = activity.getWindow().getDecorView();
-            int flags;
-
-            if (on) {
-                flags = view.getSystemUiVisibility() | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-            } else {
-                flags = view.getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
-            }
-
-            view.setSystemUiVisibility(flags);
-        }
     }
 }
